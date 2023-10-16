cbdabc671230a893a2041e2ffc3db460c4f337a0
javier1974-hub <64170309+javier1974-hub@users.noreply.github.com>
2023-10-16 20:42:39 -0300

    lee el wav, lee las marcas. Y genera ell csv de la senial en float, y las mascaras

--- Challenge/Challenge.py
+++ Challenge/Challenge.py
@@ -1,3 +1,4 @@
+import numpy as np
 import scipy.io
 from scipy.io import wavfile
 import pyqtgraph as pg
@@ -5,10 +6,10 @@ import pyqtgraph as pg
 
 
 a = scipy.io.loadmat('./annotations/hand_corrected/training-a_StateAns/a0001_StateAns.mat')
-print(a)
-print(a['state_ans'][0] )   # primera fila del diccionario
-print(a['state_ans'][0][0])   # primer elemento de la primera fila del diccionario es la posicion de la marca
-print(a['state_ans'][0][1])   # segundo elemento de la primera fila del diccionario sistole, diastolers, S2 o S2
+#print(a)
+#print(a['state_ans'][0] )   # primera fila del diccionario
+#print(a['state_ans'][0][0])   # primer elemento de la primera fila del diccionario es la posicion de la marca
+#print(a['state_ans'][0][1])   # segundo elemento de la primera fila del diccionario sistole, diastolers, S2 o S2
 
 
 ann=[]
@@ -19,14 +20,28 @@ for i in range(len(a['state_ans'])):
 
 samplerate, data = wavfile.read('./training-a/a0001.wav')
 
-win = pg.plot()
+data = data.astype(float)
+data.tofile("a0001.csv",  sep=",")
 
+win = pg.plot()
 print(samplerate)
 senial = pg.PlotDataItem(data,  pen ='w')
 win.addItem(senial)
 
-marcas = pg.PlotDataItem(data+5000)
+anotaciones = np.zeros(len(data))
 
+for i in range(len(ann)-1):
+    if (ann[i][1] == 'diastole'):
+        anotaciones[ann[i][0]:ann[(i+1)][0]] = 0
+    if (ann[i][1] == 'S1'):
+        anotaciones[ann[i][0]:ann[(i + 1)][0]] = 1
+    if (ann[i][1] == 'systole'):
+        anotaciones[ann[i][0]:ann[(i + 1)][0]] = 2
+    if (ann[i][1] == 'S2'):
+        anotaciones[ann[i][0]:ann[(i + 1)][0]] = 3
+
+anotaciones.tofile("a0001_masks.csv",  sep=",")
+marcas = pg.PlotDataItem(anotaciones*1000, pen='r')
 win.addItem(marcas)
 
 
