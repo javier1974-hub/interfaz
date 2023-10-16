40445e01825283f584204e2336659d0e4b46b424
javier1974-hub <64170309+javier1974-hub@users.noreply.github.com>
2023-10-16 11:50:09 -0300

    Cargando marcas y seniales del challenge  sigue

--- Challenge/Challenge.py
+++ Challenge/Challenge.py
@@ -1,5 +1,6 @@
 import scipy.io
-
+from scipy.io import wavfile
+import pyqtgraph as pg
 
 
 
@@ -9,7 +10,25 @@ print(a['state_ans'][0] )   # primera fila del diccionario
 print(a['state_ans'][0][0])   # primer elemento de la primera fila del diccionario es la posicion de la marca
 print(a['state_ans'][0][1])   # segundo elemento de la primera fila del diccionario sistole, diastolers, S2 o S2
 
+
+ann=[]
+
 for i in range(len(a['state_ans'])):
-    #print(a['state_ans'][i][0][0][0])
-    #print(a['state_ans'][i][1][0][0][0])
-    print(str(a['state_ans'][i][0][0][0]) + '   ' + a['state_ans'][i][1][0][0][0])
+    ann.append([a['state_ans'][i][0][0][0], a['state_ans'][i][1][0][0][0]])
+
+
+samplerate, data = wavfile.read('./training-a/a0001.wav')
+
+win = pg.plot()
+
+print(samplerate)
+senial = pg.PlotDataItem(data,  pen ='w')
+win.addItem(senial)
+
+marcas = pg.PlotDataItem(data+5000)
+
+win.addItem(marcas)
+
+
+if __name__ == "__main__":
+    pg.exec()
