69da387d4b6562524f63fc42a1a310fad521fb2b
javier1974-hub <64170309+javier1974-hub@users.noreply.github.com>
2023-10-16 21:59:55 -0300

    estarian generados todas las señales y las marcas en .csv. En la serie e de entrenamiento faltan bastantes marcas

--- Challenge/Challenge.py
+++ Challenge/Challenge.py
@@ -2,47 +2,57 @@ import numpy as np
 import scipy.io
 from scipy.io import wavfile
 import pyqtgraph as pg
+import os
 
 
+path_file = './training-e/'
+file_extention = '.wav'
+path_annotation = './annotations/hand_corrected/training-e_StateAns/'
+annotation_file_extention = '_StateAns.mat'
+prefix = 'e'
+can_files = 2141
 
-a = scipy.io.loadmat('./annotations/hand_corrected/training-a_StateAns/a0001_StateAns.mat')
-#print(a)
-#print(a['state_ans'][0] )   # primera fila del diccionario
-#print(a['state_ans'][0][0])   # primer elemento de la primera fila del diccionario es la posicion de la marca
-#print(a['state_ans'][0][1])   # segundo elemento de la primera fila del diccionario sistole, diastolers, S2 o S2
+for i in range(2092,can_files+1):
+    name = prefix + str(i).zfill(5)
+    #print(name)
+    file = name + '.wav'
+    #print(file)
+
+    a = scipy.io.loadmat(path_annotation + name + annotation_file_extention)
+
+    ann=[]
+
+    for i in range(len(a['state_ans'])):
+        ann.append([a['state_ans'][i][0][0][0], a['state_ans'][i][1][0][0][0]])
 
 
-ann=[]
+    samplerate, data = wavfile.read(path_file + name + file_extention)
 
-for i in range(len(a['state_ans'])):
-    ann.append([a['state_ans'][i][0][0][0], a['state_ans'][i][1][0][0][0]])
+    data = data.astype(float)
+    data.tofile(name + '.csv',  sep=",")
+
+    anotaciones = np.zeros(len(data))
+
+    for i in range(len(ann)-1):
+        if (ann[i][1] == 'diastole'):
+            anotaciones[ann[i][0]:ann[(i+1)][0]] = 0
+        if (ann[i][1] == 'S1'):
+            anotaciones[ann[i][0]:ann[(i + 1)][0]] = 1
+        if (ann[i][1] == 'systole'):
+            anotaciones[ann[i][0]:ann[(i + 1)][0]] = 2
+        if (ann[i][1] == 'S2'):
+            anotaciones[ann[i][0]:ann[(i + 1)][0]] = 3
+
+    anotaciones.tofile(name + '_mask.csv',  sep=",")
 
 
-samplerate, data = wavfile.read('./training-a/a0001.wav')
-
-data = data.astype(float)
-data.tofile("a0001.csv",  sep=",")
-
-win = pg.plot()
-print(samplerate)
-senial = pg.PlotDataItem(data,  pen ='w')
-win.addItem(senial)
-
-anotaciones = np.zeros(len(data))
-
-for i in range(len(ann)-1):
-    if (ann[i][1] == 'diastole'):
-        anotaciones[ann[i][0]:ann[(i+1)][0]] = 0
-    if (ann[i][1] == 'S1'):
-        anotaciones[ann[i][0]:ann[(i + 1)][0]] = 1
-    if (ann[i][1] == 'systole'):
-        anotaciones[ann[i][0]:ann[(i + 1)][0]] = 2
-    if (ann[i][1] == 'S2'):
-        anotaciones[ann[i][0]:ann[(i + 1)][0]] = 3
-
-anotaciones.tofile("a0001_masks.csv",  sep=",")
-marcas = pg.PlotDataItem(anotaciones*1000, pen='r')
-win.addItem(marcas)
+# win = pg.plot()
+# print(samplerate)
+# senial = pg.PlotDataItem(data,  pen ='w')
+# win.addItem(senial)
+#
+# marcas = pg.PlotDataItem(anotaciones*1000, pen='r')
+# win.addItem(marcas)
 
 
 if __name__ == "__main__":
