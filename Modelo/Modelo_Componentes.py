c247fba0c0f5bc7350e3ebe3a892b7903a072345
javier1974-hub <64170309+javier1974-hub@users.noreply.github.com>
2023-12-20 19:49:06 -0300

    Estaria funcionando el asunto de la kurtosis

--- Modelo/Modelo_Componentes.py
+++ Modelo/Modelo_Componentes.py
@@ -3,6 +3,7 @@ from scipy.io import wavfile
 import pyqtgraph as pg
 import scipy as sp
 import scipy.signal as sig
+from scipy.stats import kurtosis
 
 def nco(fcw, sr):
     phase =  0
@@ -78,11 +79,27 @@ def S(frec1, frec2,frec3,Dt1, Dt2, Dt3, A1, A2, A3, fs):
             ph_step = 2 * np.pi * frec3 * 1 / fs
             phase += ph_step
             Sonido.append(A3 * np.cos(phase))
-
-
     return Sonido
 
-S1=S(5,20,5,2,2,2, 0.2,1,0.1,1000)
+S1=S(3,4,5,3,1,3, 0.8,1,0.8,1000)
+Window = sig.windows.gaussian(500, 10)
+S1_2 = sig.filtfilt(Window, np.sum(Window), S1)
+
+Window1 = sig.windows.gaussian(len(S1), len(S1)/15)
+S1_3 = Window1 * S1
+
+S1_3_array = np.asarray(S1_3)
+k = kurtosis(S1_3)
+print(k)
+
+window_kurt = 10
+# con 30 ya no se ve
+kurt = []
+
+for i in range(1,(len(S1_3)-window_kurt)):
+    kurt.append( kurtosis(S1_3_array[i:(i + window_kurt)]))
+
+
 
 
 # sr = 2000
@@ -117,13 +134,24 @@ legend.setParentItem(win.graphicsItem())
 
 s1 = pg.PlotDataItem(S1,  pen ='w')
 win.addItem(s1)
-legend.addItem(s1,'Filtrado')
+legend.addItem(s1,'Sin Filtrar')
 
+#s1_2 = pg.PlotDataItem(S1_2,  pen ='r')
+#win.addItem(s1_2)
+#legend.addItem(s1_2,'Filtrado Gauss')
 
-# Result = pg.PlotDataItem(result,  pen ='w')
-# win.addItem(Result)
-# legend.addItem(Result,'Filtrado')
-#
+s1_3 = pg.PlotDataItem(S1_3,  pen ='r')
+win.addItem(s1_3)
+legend.addItem(s1_3,'Mult.  Gauss')
+
+window = pg.PlotDataItem(Window,  pen ='c')
+win.addItem(window)
+legend.addItem(window,'window')
+
+Kurt = pg.PlotDataItem(kurt,  pen ='m')
+win.addItem(Kurt)
+legend.addItem(Kurt,'kurtosis')
+
 # Result1 = pg.PlotDataItem(result1,  pen ='r')
 # win.addItem(Result1)
 # legend.addItem(Result1,'Sin Filtrar')
